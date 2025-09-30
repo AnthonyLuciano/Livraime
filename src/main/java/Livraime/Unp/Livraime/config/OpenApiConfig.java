2cbd04c61721c14b4f36d679594bddc9871a6cee
Anthony <102555390+AnthonyLuciano@users.noreply.github.com>
2025-09-30 16:08:59 -0300

    Create OpenApiConfig.java

---
+++ src/main/java/Livraime/Unp/Livraime/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package Livraime.Unp.Livraime.config;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class OpenApiConfig {
+     @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Livrai-me API")
+                        .description("API do sistema Livrai-me (assinaturas, livros, beneficiários e autores)")
+                        .version("1.0.0"));
+    }
+}
